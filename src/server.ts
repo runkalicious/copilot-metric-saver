de309e9f243874924677de11191c9133738ec819
Junqian Zhuang <junqianz@microsoft.com>
2024-09-14 22:56:06 +0800

    add schedule jobs in server.ts. fetch data and save it each 12 hours.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 import express from 'express';
 import { UsageServiceFactory } from './api/UsageServiceFactory';
 import cors from 'cors';
+
   
 const usageService=UsageServiceFactory.createUsageService();
 
@@ -9,6 +10,27 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(cors());
+
+
+// define the job, then run scheduled job every 12 hours
+const runJob = async () => {
+    console.log('Running a job');
+    try {
+        await usageService.saveUsageData();
+        // get current time and log it.
+        const now = new Date();
+        console.log(`Data saved successfully at ${now}`);
+    } catch (error) {
+        const now = new Date();
+        console.error(`Error saving usage data at ${now}:`, error);
+    }
+};
+
+// run it once the server starts
+runJob();
+
+// run job every 12 hours
+setInterval(runJob, 12 * 60 * 60 * 1000);
 
 // redirect default to /metrics/service
 app.get('/', (req, res) => {
@@ -27,6 +49,7 @@ app.get('/metrics/service', async (req, res) => {
         res.status(500).send('Error fetching metrics from storage');
     }
 });
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
